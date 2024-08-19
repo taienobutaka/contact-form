b5741cdbb5ff2890f4fb16e59e99abf19fa4346c
taie <taie0424engineer@gmail.com>
2024-08-19 12:27:53 +0900

    リモートリポジトリの変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::get('/', [ContactController::class, 'index']);
 
 /*
 |--------------------------------------------------------------------------
